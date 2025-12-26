3309590af1525179cf4cbbc599b067e91d38a827
gurling <owpender@gmail.com>
2025-12-26 21:05:30 +0100

    Update install-ironbar.sh

--- files/scripts/install-ironbar.sh
+++ files/scripts/install-ironbar.sh
@@ -8,12 +8,15 @@ dnf -y install rust cargo git luajit lua-lgi  gtk4 gtk-layer-shell gtk4-devel gt
 
 git clone https://github.com/jakestanger/ironbar.git
 cd ironbar
-cargo build --release --no-default-features \
-	--features config+all,cli,battery,bluetooth,clock,label,tray,volume,sys_info,notifications,workspaces+niri
+#cargo build --release --no-default-features \
+#	--features config+all,cli,battery,bluetooth,clock,label,tray,volume,sys_info,notifications,workspaces+niri,focused
+cargo build --locked --release 
 # change path to wherever you want to install
 install target/release/ironbar /usr/bin/ironbar && echo "Ironbar Successfully installed."
 
-dnf -y remove rust cargo git gtk4-devel gtk4-layer-shell-devel dbus-devel openssl-devel pulseaudio-libs-devel libinput-devel luajit-devel
+dnf -y remove rust cargo git gtk4-devel gtk4-layer-shell-devel dbus-devel openssl-devel pulseaudio-libs-devel libinput-devel luajit-devel dbus-devel
+
+dnf -y install gtk-layer-shell luajit lua-lgi pkgconf-pkg-config
 
 rm -rf /root/.cargo/registry /root/.cargo/git /root/ionbar || true
 #docker run -it -v '.:/app' ghcr.io/jakestanger/ironbar-build /bin/bash
