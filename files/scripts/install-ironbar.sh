d3f4a2f190bcc93b9a06fec7b2820f1d0d241dee
gurling <owpender@gmail.com>
2025-12-26 13:25:23 +0100

    Update install-ironbar.sh

--- files/scripts/install-ironbar.sh
+++ files/scripts/install-ironbar.sh
@@ -2,11 +2,21 @@
 
 set -euo pipefail
 
-cargo install ironbar \
-	--locked \
-	--root /usr \
+OPENSSL_DIR="/usr/bin/openssl"
 
-rm -rf /root/.cargo/registry /root/.cargo/git || true
+git clone https://github.com/jakestanger/ironbar.git
+cd ironbar
+cargo build --release
+# change path to wherever you want to install
+install target/release/ironbar /usr/bin/ironbar
+
+
+#cargo install ironbar \
+#	--locked \
+#	--root /usr \
+#
+
+rm -rf /root/.cargo/registry /root/.cargo/git /root/ionbar || true
 
 
 #docker run -it -v '.:/app' ghcr.io/jakestanger/ironbar-build /bin/bash
