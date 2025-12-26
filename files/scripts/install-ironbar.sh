44a59a383240873464fbf98bf87f28bcf60ac97b
gurling <owpender@gmail.com>
2025-12-26 13:54:17 +0100

    Update install-ironbar.sh

--- files/scripts/install-ironbar.sh
+++ files/scripts/install-ironbar.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 #OPENSSL_DIR="/usr/bin"
 
+dnf -y install rust cargo git luajit lua-lgi  gtk4 gtk-layer-shell gtk4-devel gtk4-layer-shell-devel dbus-devel pkgconf-pkg-config openssl-devel pulseaudio-libs-devel libinput-devel luajit-devel
+
 git clone https://github.com/jakestanger/ironbar.git
 cd ironbar
 cargo build --release
@@ -18,6 +20,7 @@ install target/release/ironbar /usr/bin/ironbar
 
 rm -rf /root/.cargo/registry /root/.cargo/git /root/ionbar || true
 
+sudo dnf -y remove rust cargo git gtk4-devel gtk4-layer-shell-devel dbus-devel pkgconf-pkg-config openssl-devel pulseaudio-libs-devel libinput-devel luajit-devel
 
 #docker run -it -v '.:/app' ghcr.io/jakestanger/ironbar-build /bin/bash
 #cd app
